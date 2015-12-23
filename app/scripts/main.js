a5059785248b652454fbbc3ac95ac0e68d48b97d
MartinKilonzo <martinkilonzo2@gmail.com>
2015-12-22 19:56:29 -0500

    Setup plugins (ScrollMagic, NiceScroll, GSAP) and frameworked parallax

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,7 +5,8 @@ console.log('\'Allo \'Allo!');
 // Building the pages components
 $(function() {
 	$('#header').load('partials/header.html');
-	$('#content').load('partials/content.html');
+	$('#content').load('partials/test.html');
 	$('#footer').load('partials/footer.html');
+	$('html').niceScroll();
 });
 }());
--- app/scripts/scrolling.js
+++ app/scripts/scrolling.js
@@ -1,4 +1,27 @@
 (function () {
-'use strict';
-console.log('Scrolling online!');
+	'use strict';
+
+	// Controller information
+	console.info('Scrolling online!');
+	console.info('----- PLUGIN INFO -----');
+	console.info('TweenMax (GSAP) Version:', TweenMax.version);
+	console.info('ScrollMagic Version:', ScrollMagic.version);
+	console.info('ScrollMagic (GSAP) Version:', ScrollMagic.version);
+	console.info('----- PLUGIN INFO -----\n');
+
+	// init controller
+	var sceneController = new ScrollMagic.Controller();
+	// var redContainerScene = new ScrollMagic.Scene({
+	// 	triggerElement: '#red-scene',
+	// 	duration: 100
+	// })
+	// .setTween('#red-block', 0.1, {
+	// 	backgroundColor: 'red',
+	// 	color: 'white',
+	// 	ease: Linear.easeNone
+	// })
+	// .addIndicators({name: "red"})
+	// .addTo(sceneController);
+
+	//Parallax Scenes
 }());
